828adec8eb60e49db561c8ee32548d2c7aa49423
sumitpo <2797366715@qq.com>
2024-12-27 20:21:58 +0800

    feat: bst solve (not finished)

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,71 @@
 #ifndef BST_H
 #define BST_H
 
+#include <functional>
+#include <iostream>
 
-#endif //BST_H
+class BST {
+public:
+  class Node {
+  public:
+    Node(int value, Node *left, Node *right);
+    Node() {
+      value = 0;
+      left = nullptr;
+      right = nullptr;
+    }
+    Node(const Node &node);
+
+    int value;
+    Node *left;
+    Node *right;
+    friend std::ostream &operator<<(std::ostream &os, const Node &n) {
+      os << n.value << std::endl;
+      if (n.left != nullptr) {
+        std::cout << n.left << std::endl;
+      }
+      if (n.right != nullptr) {
+        std::cout << n.right << std::endl;
+      }
+      return os;
+    }
+    bool operator<(const int &n) { return this->value < n; }
+    friend bool operator<(int lv, const Node &n) { return lv < n.value; }
+    bool operator>(const int &n) { return this->value > n; }
+    friend bool operator>(int lv, const Node &n) { return lv > n.value; }
+    bool operator>=(const int &n) { return this->value >= n; }
+    friend bool operator>=(int lv, const Node &n) { return lv >= n.value; }
+    bool operator<=(const int &n) { return this->value <= n; }
+    friend bool operator<=(int lv, const Node &n) { return lv <= n.value; }
+    bool operator==(const int &n) { return this->value == n; }
+    friend bool operator==(int lv, const Node &n) { return lv == n.value; }
+  };
+
+  BST(std::initializer_list<int> list) : root(nullptr) {
+    len = 0;
+    for (int value : list) {
+      add_node(value);
+    }
+  }
+
+  Node *&get_root();
+  void bfs(std::function<void(Node *&node)> func);
+  size_t length();
+  bool add_node(int value);
+  Node **find_node(int value);
+  Node **find_parrent(int value);
+  Node **find_successor(int value);
+  bool delete_node(int value);
+  friend std::ostream &operator<<(std::ostream &os, const BST &n) {
+    os << n.root << std::endl;
+    return os;
+  }
+  BST &operator++() { return *this; }
+  BST &operator++(int v) { return *this; }
+
+private:
+  Node *root;
+  size_t len;
+};
+
+#endif // BST_H
--- src/bst.cpp
+++ src/bst.cpp
@@ -1,1 +1,111 @@
 #include "bst.h"
+#include <queue>
+
+BST::Node::Node(int value, BST::Node *left, BST::Node *right) {
+  this->value = value;
+  this->left = left;
+  this->right = right;
+}
+
+BST::Node::Node(const BST::Node &node) {
+  this->value = node.value;
+  this->left = node.left;
+  this->right = node.right;
+}
+
+BST::Node *&BST::get_root() { return root; }
+bool BST::add_node(int value) {
+  BST::Node **node_ptr = find_node(value);
+  if (node_ptr != nullptr) {
+    return false;
+  }
+  BST::Node *new_node_ptr = new BST::Node(value, nullptr, nullptr);
+  if (root == nullptr) {
+    root = new_node_ptr;
+    len += 1;
+    return true;
+  }
+  node_ptr = find_parrent(value);
+  if ((*node_ptr)->value < value) {
+    (*node_ptr)->right = new_node_ptr;
+  } else {
+    (*node_ptr)->left = new_node_ptr;
+  }
+  len += 1;
+  return true;
+}
+
+BST::Node **BST::find_node(int value) {
+  if (root == nullptr)
+    return nullptr;
+  if (root->value == value) {
+    return &root;
+  }
+  BST::Node **curr = &root;
+  while ((*curr) != nullptr) {
+    if ((*curr)->value < value) {
+      curr = &((*curr)->right);
+    } else if ((*curr)->value == value) {
+      return curr;
+    } else {
+      curr = &((*curr)->left);
+    }
+  }
+  return nullptr;
+}
+BST::Node **BST::find_parrent(int value) {
+  if (root == nullptr) {
+    return nullptr;
+  }
+  if (root->value == value) {
+    return &root;
+  }
+  BST::Node **par = &root, **curr = &root;
+  while ((*curr) != nullptr) {
+    if ((*curr)->value < value) {
+      par = curr;
+      curr = &((*curr)->right);
+    } else if ((*curr)->value == value) {
+      return par;
+    } else {
+      par = curr;
+      curr = &((*curr)->left);
+    }
+  }
+  return par;
+}
+BST::Node **BST::find_successor(int value) { return &root; }
+bool BST::delete_node(int value) {
+  BST::Node **node_ptr = find_node(value);
+  if (nullptr == node_ptr) {
+    return false;
+  }
+  BST::Node **par = find_parrent(value);
+  BST::Node *node = *node_ptr;
+  (*par)->left = node->left;
+  delete node;
+  *node_ptr = nullptr;
+  len -= 1;
+  return true;
+}
+
+void BST::bfs(std::function<void(BST::Node *&node)> func) {
+  std::queue<BST::Node *> que;
+  que.push(root);
+  while (!que.empty()) {
+    BST::Node *curr = que.front();
+    que.pop();
+    if (curr == nullptr) {
+      continue;
+    }
+    func(curr);
+    if (curr->left != nullptr) {
+      que.push(curr->left);
+    }
+    if (curr->right != nullptr) {
+      que.push(curr->right);
+    }
+  }
+}
+
+size_t BST::length() { return len; }
